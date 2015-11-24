45f225a36ac27b2fdc568e2df64927f36ebfdadf
FedericoFdez <fa.fernandez@alumnos.upm.es>
2015-11-24 09:13:32 +0100

    Initial Commit

---
+++ pokedex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex
+//
+//  Created by Federico Fernández on 24/11/15.
+//  Copyright © 2015 Federico Fernández. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
